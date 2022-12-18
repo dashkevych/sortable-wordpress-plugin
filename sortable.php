9a156e224612913975767681c4d7a1a2fb391de5
Taras Dashkevych <trdashix@gmail.com>
2022-12-17 19:05:43 -0800

    Update author

--- sortable.php
+++ sortable.php
@@ -5,7 +5,8 @@
  * Requires at least: 6.1
  * Requires PHP:      7.4
  * Version:           1.0.0
- * Author:            Taras Dashkevych
+ * Author:            Themes Harbor
+ * Author URI:        https://themesharbor.com/
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       sortable
