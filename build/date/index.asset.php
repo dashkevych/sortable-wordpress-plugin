dd4d8041ce062c09df75cac0b64044b7df9d746f
Taras Dashkevych <trdashix@gmail.com>
2022-12-30 16:45:22 -0800

    new version: 1.1.0

--- build/date/index.asset.php
+++ build/date/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '7bf3857f0bd2dc13accc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '7ba6bf8fd6b596ac96f3');
--- build/entry/index.asset.php
+++ build/entry/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3a41706be2356a4b3e1e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1be00aac42256cda981e');
