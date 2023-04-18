6daea6bd09c8ac732ae711b43a913766141b52cb
Taras Dashkevych <trdashix@gmail.com>
2023-04-18 12:09:27 -0700

    * Update: formatting;

--- src/entry/edit.js
+++ src/entry/edit.js
@@ -3,7 +3,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -17,14 +17,14 @@ import {
 	useInnerBlocksProps,
 	InspectorControls,
 	store as blockEditorStore,
-} from '@wordpress/block-editor';
+} from "@wordpress/block-editor";
 
 /**
  * This package includes a library of generic WordPress components.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
  */
-import { DateTimePicker, PanelBody, PanelRow } from '@wordpress/components';
+import { DateTimePicker, PanelBody, PanelRow } from "@wordpress/components";
 
 /**
  * WordPress dependencies.
@@ -32,8 +32,14 @@ import { DateTimePicker, PanelBody, PanelRow } from '@wordpress/components';
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-element/ ,
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
  */
-import { useEffect } from '@wordpress/element';
-import { useSelect, useDispatch } from '@wordpress/data';
+import { useEffect } from "@wordpress/element";
+
+/**
+ * WordPress Data API functions for managing application data.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
+ */
+import { useSelect, useDispatch } from "@wordpress/data";
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -41,7 +47,7 @@ import { useSelect, useDispatch } from '@wordpress/data';
  *
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
-import './editor.scss';
+import "./editor.scss";
 
 /**
  * Allowed blocks constant is passed to InnerBlocks precisely as specified here.
@@ -53,13 +59,13 @@ import './editor.scss';
  * @type {string[]}
  */
 const ALLOWED_BLOCKS = [
-	'sortable/date',
-	'core/heading',
-	'core/paragraph',
-	'core/media-text',
-	'core/group',
-	'core/columns',
-	'core/block',
+	"sortable/date",
+	"core/heading",
+	"core/paragraph",
+	"core/media-text",
+	"core/group",
+	"core/columns",
+	"core/block",
 ];
 
 /**
@@ -71,73 +77,72 @@ const ALLOWED_BLOCKS = [
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  */
-export default function Edit( props ) {
+export default function Edit(props) {
 	const {
 		clientId,
 		attributes: { dateTime },
 		setAttributes,
 	} = props;
 	const blockProps = useBlockProps();
-	const { updateBlockAttributes } = useDispatch( blockEditorStore );
+	const { updateBlockAttributes } = useDispatch(blockEditorStore);
 
 	const { rootClientId, hasChildBlocks } = useSelect(
-		( select ) => {
-			const { getBlockRootClientId, getBlockOrder } =
-				select( blockEditorStore );
-			const rootId = getBlockRootClientId( clientId );
+		(select) => {
+			const { getBlockRootClientId, getBlockOrder } = select(blockEditorStore);
+			const rootId = getBlockRootClientId(clientId);
 
 			return {
 				rootClientId: rootId,
-				hasChildBlocks: getBlockOrder( clientId ).length > 0,
+				hasChildBlocks: getBlockOrder(clientId).length > 0,
 			};
 		},
-		[ clientId ]
+		[clientId]
 	);
 
 	// Update date in block.
-	const updateDate = ( value ) => {
-		setAttributes( { dateTime: value } );
+	const updateDate = (value) => {
+		setAttributes({ dateTime: value });
 
 		// Reset parent block sorting.
-		updateBlockAttributes( rootClientId, {
-			orderBy: '',
-		} );
+		updateBlockAttributes(rootClientId, {
+			orderBy: "",
+		});
 	};
 
-	useEffect( () => {
-		if ( ! dateTime ) {
-			setAttributes( { dateTime: new Date() } );
+	useEffect(() => {
+		if (!dateTime) {
+			setAttributes({ dateTime: new Date() });
 
 			// Reset parent block sorting.
-			updateBlockAttributes( rootClientId, {
-				orderBy: '',
-			} );
+			updateBlockAttributes(rootClientId, {
+				orderBy: "",
+			});
 		}
-	}, [ dateTime ] );
+	}, [dateTime]);
 
-	const innerBlocksProps = useInnerBlocksProps( blockProps, {
+	const innerBlocksProps = useInnerBlocksProps(blockProps, {
 		allowedBlocks: ALLOWED_BLOCKS,
-		orientation: 'horizontal',
+		orientation: "horizontal",
 		renderAppender: hasChildBlocks
 			? undefined
 			: InnerBlocks.ButtonBlockAppender,
-	} );
+	});
 
 	return (
 		<>
 			<InspectorControls>
-				<PanelBody title={ __( 'Date Settings', 'sortable' ) }>
+				<PanelBody title={__("Date Settings", "sortable")}>
 					<PanelRow className="sortable-block__date-row">
 						<DateTimePicker
-							currentDate={ dateTime }
-							onChange={ updateDate }
-							is12Hour={ true }
+							currentDate={dateTime}
+							onChange={updateDate}
+							is12Hour={true}
 						/>
 					</PanelRow>
 				</PanelBody>
 			</InspectorControls>
 
-			<div { ...innerBlocksProps } />
+			<div {...innerBlocksProps} />
 		</>
 	);
 }
