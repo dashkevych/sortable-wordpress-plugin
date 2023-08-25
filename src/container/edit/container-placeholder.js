19030c88c0be5b39824d1460d71f80206b243aca
Taras Dashkevych <trdashix@gmail.com>
2023-08-25 15:52:37 -0700

    * Changes: * Add: support for pre-defined patterns; * Update: refactor welcome screen when block first added to content area; * Update: formatting in files;

--- src/container/edit/container-placeholder.js
+++ src/container/edit/container-placeholder.js
@@ -1,18 +1,25 @@
 /**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from "@wordpress/i18n";
+
+/**
  * Import necessary modules from the WordPress Block Editor
  */
 import {
 	__experimentalBlockVariationPicker,
 	useBlockProps,
 	store as blockEditorStore,
-} from '@wordpress/block-editor';
+} from "@wordpress/block-editor";
 
 /**
  * WordPress Data API functions for managing application data.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
  */
-import { useDispatch, useSelect } from '@wordpress/data';
+import { useDispatch, useSelect } from "@wordpress/data";
 
 /**
  * This module imports hooks from the WordPress Element package that allow React components to
@@ -21,7 +28,7 @@ import { useDispatch, useSelect } from '@wordpress/data';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-element/
  */
-import { useState } from '@wordpress/element';
+import { useState } from "@wordpress/element";
 
 /**
  * Import functions and store from WordPress Blocks package.
@@ -29,72 +36,112 @@ import { useState } from '@wordpress/element';
 import {
 	createBlocksFromInnerBlocksTemplate,
 	store as blocksStore,
-} from '@wordpress/blocks';
+} from "@wordpress/blocks";
+
+/**
+ * WordPress core components.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/components/
+ */
+import { Button, Placeholder } from "@wordpress/components";
 
 // Import ContainerContent component
-import ContainerContent from './container-content';
+import ContainerContent from "./container-content";
 
 /**
  * ContainerPlaceholder component
  *
  * @param props
  */
-export default function ContainerPlaceholder( props ) {
-	const { clientId, name, setAttributes } = props;
+export default function ContainerPlaceholder(props) {
+	const { clientId, name, openPatternSelectionModal, setAttributes } = props;
+
+	// Define the phases
+	const PHASES = {
+		PICKER: "picker",
+		BLANK: "blank",
+		CONTENT: "content",
+	};
+
+	// Replace isPickerVisible and isStartingBlank with currentPhase
+	const [currentPhase, setCurrentPhase] = useState(PHASES.PICKER);
 
 	const { blockType, defaultVariation, variations } = useSelect(
-		( select ) => {
-			const {
-				getBlockVariations,
-				getBlockType,
-				getDefaultBlockVariation,
-			} = select( blocksStore );
+		(select) => {
+			const { getBlockVariations, getBlockType, getDefaultBlockVariation } =
+				select(blocksStore);
 
 			return {
-				blockType: getBlockType( name ),
-				defaultVariation: getDefaultBlockVariation( name, 'block' ),
-				variations: getBlockVariations( name, 'block' ),
+				blockType: getBlockType(name),
+				defaultVariation: getDefaultBlockVariation(name, "block"),
+				variations: getBlockVariations(name, "block"),
 			};
 		},
-		[ name, clientId ]
+		[name, clientId]
 	);
-	const { replaceInnerBlocks } = useDispatch( blockEditorStore );
+	const { replaceInnerBlocks } = useDispatch(blockEditorStore);
 	const blockProps = useBlockProps();
 
-	const [ isPickerVisible, setIsPickerVisible ] = useState( true );
-
-	if ( ! isPickerVisible ) {
-		return <ContainerContent { ...props } />;
-	}
-
-	return (
-		<div { ...blockProps }>
+	// Check if the user want to use the Variation Picker when first use block.
+	if (currentPhase === PHASES.BLANK) {
+		return (
 			<__experimentalBlockVariationPicker
-				icon={ blockType?.icon?.src }
-				label={ blockType?.title }
-				variations={ variations }
-				onSelect={ ( nextVariation = defaultVariation ) => {
+				icon={blockType?.icon?.src}
+				label={blockType?.title}
+				variations={variations}
+				onSelect={(nextVariation = defaultVariation) => {
 					// Handle the "Skip" button click here. You can either do nothing or perform some custom action.
-					if ( ! nextVariation.innerBlocks.length ) {
-						setIsPickerVisible( false );
+					if (!nextVariation.innerBlocks.length) {
+						setCurrentPhase(PHASES.CONTENT);
 						return;
 					}
 
-					if ( nextVariation.attributes ) {
-						setAttributes( nextVariation.attributes );
+					if (nextVariation.attributes) {
+						setAttributes(nextVariation.attributes);
 					}
 
-					if ( nextVariation.innerBlocks ) {
+					if (nextVariation.innerBlocks) {
 						const innerBlocksTemplate = nextVariation.innerBlocks;
 						const createdBlocks =
-							createBlocksFromInnerBlocksTemplate(
-								innerBlocksTemplate
-							);
-						replaceInnerBlocks( clientId, createdBlocks, true );
+							createBlocksFromInnerBlocksTemplate(innerBlocksTemplate);
+						replaceInnerBlocks(clientId, createdBlocks, true);
 					}
-				} }
+
+					setCurrentPhase(PHASES.CONTENT);
+				}}
 				allowSkip
 			/>
+		);
+	}
+
+	if (currentPhase === PHASES.CONTENT) {
+		return <ContainerContent {...props} />;
+	}
+
+	const hasPatterns = true;
+
+	return (
+		<div {...blockProps}>
+			<Placeholder
+				icon={blockType?.icon?.src}
+				label={blockType?.title}
+				instructions={__("Choose a pattern or start blank.")}
+			>
+				{!!hasPatterns && (
+					<Button variant="primary" onClick={openPatternSelectionModal}>
+						{__("Choose")}
+					</Button>
+				)}
+
+				<Button
+					variant="secondary"
+					onClick={() => {
+						setCurrentPhase(PHASES.BLANK);
+					}}
+				>
+					{__("Start blank")}
+				</Button>
+			</Placeholder>
 		</div>
 	);
 }
--- src/container/edit/index.js
+++ src/container/edit/index.js
@@ -4,32 +4,58 @@
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
  */
-import { store as blockEditorStore } from '@wordpress/block-editor';
+import { store as blockEditorStore } from "@wordpress/block-editor";
 
 /**
  * WordPress Data API functions for managing application data.
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-data/
  */
-import { useSelect } from '@wordpress/data';
+import { useSelect } from "@wordpress/data";
+
+/**
+ * This module imports hooks from the WordPress Element package that allow React components to
+ * manage state and handle side effects. These hooks provide basic building blocks for building
+ * powerful, dynamic interfaces in WordPress.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-element/
+ */
+import { useState } from "@wordpress/element";
 
 /**
  * Internal dependencies
  */
-import ContainerContent from './container-content';
-import ContainerPlaceholder from './container-placeholder';
+import ContainerContent from "./container-content";
+import ContainerPlaceholder from "./container-placeholder";
+import PatternSelectionModal from "./pattern-selection-modal";
 
-const ContainerEdit = ( props ) => {
-	const { clientId } = props;
+const ContainerEdit = (props) => {
+	const { clientId, attributes } = props;
+	const [isPatternSelectionModalOpen, setIsPatternSelectionModalOpen] =
+		useState(false);
 	const hasInnerBlocks = useSelect(
-		( select ) =>
-			!! select( blockEditorStore ).getBlocks( clientId ).length,
-		[ clientId ]
+		(select) => !!select(blockEditorStore).getBlocks(clientId).length,
+		[clientId]
 	);
 
 	const Component = hasInnerBlocks ? ContainerContent : ContainerPlaceholder;
 
-	return <Component { ...props } />;
+	return (
+		<>
+			<Component
+				{...props}
+				openPatternSelectionModal={() => setIsPatternSelectionModalOpen(true)}
+			/>
+
+			{isPatternSelectionModalOpen && (
+				<PatternSelectionModal
+					clientId={clientId}
+					attributes={attributes}
+					setIsPatternSelectionModalOpen={setIsPatternSelectionModalOpen}
+				/>
+			)}
+		</>
+	);
 };
 
 export default ContainerEdit;
