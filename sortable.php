1861446ead02f7544b8550ae4c5fa1e8f2c04364
Taras Dashkevych <trdashix@gmail.com>
2023-10-07 11:00:10 -0700

    * Add: server-side rendering support for Sortable Entry block;

--- sortable.php
+++ sortable.php
@@ -29,10 +29,18 @@ function sortable_plugin_init() {
 			'render_callback' => 'sortable_render_block_sortable_date',
 		)
 	);
+
 	// Register Sortable Entry block.
-	register_block_type( __DIR__ . '/build/entry' );
+	register_block_type(
+		__DIR__ . '/build/entry',
+		array(
+			'render_callback' => 'sortable_render_block_sortable_entry',
+		)
+	);
+
 	// Register Sortable block.
 	register_block_type( __DIR__ . '/build/container' );
+
 	// Register Pattern.
 	register_block_pattern(
 		'sortable/changelog-layout-1',
@@ -202,4 +210,66 @@ function sortable_render_block_sortable_date( $attributes, $content, $block ) {
 		$unformatted_date,
 		$formatted_date
 	);
+}
+
+/**
+ * Render callback function for the Sortable Entry block.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Return block's content.
+ */
+function sortable_render_block_sortable_entry( $attributes, $content, $block ) {
+    // Access block context using the context property of the $block object.
+    $context = $block->context;
+
+    // Use the $context variable to access contextual information.
+    $filter_settings = isset( $context['sortable/filter'] ) ? $context['sortable/filter'] : null;
+
+    // Extract filter settings.
+    $filter_date_value = isset( $filter_settings['date'] ) ? $filter_settings['date'] : '';
+    $filter_date_type = isset( $filter_settings['dateFilterType'] ) ? $filter_settings['dateFilterType'] : 'on';
+    $include_date_time = isset( $filter_settings['includeDateTime'] ) ? $filter_settings['includeDateTime'] : false;
+
+    // Determine if block is outdated.
+    $is_outdated = false;
+
+    // Only check for outdated blocks if filter date value is not empty.
+    if ( !empty($filter_date_value) ) {
+        $block_date = new DateTime( $attributes['dateTime'] );
+        $filter_date = new DateTime( $filter_date_value );
+
+        // Reset time to midnight if includeDateTime is false.
+        if ( ! $include_date_time ) {
+            $block_date->setTime(0, 0, 0);
+            $filter_date->setTime(0, 0, 0);
+        }
+
+        if (
+            ($filter_date_type === 'on' && $block_date != $filter_date) ||
+            ($filter_date_type === 'after' && $block_date <= $filter_date) ||
+            ($filter_date_type === 'before' && $block_date >= $filter_date)
+        ) {
+            $is_outdated = true;
+        }
+    }
+
+    // Prepare wrapper attributes.
+    $wrapper_attributes = get_block_wrapper_attributes( array(
+        'data-event-date' => esc_attr( $attributes['dateTime'] ),
+        'class' => $is_outdated ? 'is-outdated' : '',
+    ) );
+
+	if ( $is_outdated ) {
+		return false;
+	}
+
+    // Return block content with wrapper attributes.
+    return sprintf(
+        '<div %1$s>%2$s</div>',
+        $wrapper_attributes,
+        $content
+    );
 }
