3fc2ddca009dd56f18fde5851e34ff330f32a900
Taras Dashkevych <trdashix@gmail.com>
2023-10-10 13:37:31 -0700

    * Update: refactor render for block specific patterns;

--- sortable.php
+++ sortable.php
@@ -41,142 +41,25 @@ function sortable_plugin_init() {
 	// Register Sortable block.
 	register_block_type( __DIR__ . '/build/container' );
 
-	// Register Pattern.
-	register_block_pattern(
-		'sortable/changelog-layout-1',
-		array(
-			'title'       => esc_html__( 'Changelog List', 'sortable' ),
-			'description' => _x( 'Layout to create changelog list with version number, date, and list.', 'Block pattern description', 'sortable' ),
-			'blockTypes'  => array( 'sortable/container' ),
-			'content'     => '<!-- wp:sortable/container -->
-			<div class="wp-block-sortable-container is-list">
-				<!-- wp:sortable/entry {"dateTime":"2023-08-28T10:21:00"} -->
-				<div class="wp-block-sortable-entry">
-					<!-- wp:group {"style":{"spacing":{"blockGap":"1.5em"}},"layout":{"type":"constrained"}} -->
-					<div class="wp-block-group">
-						<!-- wp:separator {"backgroundColor":"cyan-bluish-gray"} -->
-						<hr class="wp-block-separator has-text-color has-cyan-bluish-gray-color has-alpha-channel-opacity has-cyan-bluish-gray-background-color has-background" />
-						<!-- /wp:separator -->
-						<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-						<div class="wp-block-group">
-							<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-							<div class="wp-block-group">
-								<!-- wp:paragraph -->
-								<p><strong>1.1.0</strong></p>
-								<!-- /wp:paragraph -->
-								<!-- wp:sortable/date {"textColor":"vivid-purple"} /-->
-							</div>
-							<!-- /wp:group -->
-							<!-- wp:list {"fontSize":"normal"} -->
-							<ul class="has-normal-font-size">
-								<!-- wp:list-item -->
-								<li><strong>Add:</strong> support for colors in Sortable Date block;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Add:</strong> support for padding dimensions in Sortable Date block;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> improve Insert buttons in the editor;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> detect time format in Sortable Date block;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> allow Group block in Sortable Entry;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Fix:</strong> style back-end issues;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Fix:</strong> button contrast issue in Sortable Container;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Fix:</strong> wrong time output in Sortable Date block when viewed on front-end;</li>
-								<!-- /wp:list-item -->
-							</ul>
-							<!-- /wp:list -->
-						</div>
-						<!-- /wp:group -->
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:sortable/entry -->
-				<!-- wp:sortable/entry {"dateTime":"2023-07-12T10:21:00"} -->
-				<div class="wp-block-sortable-entry">
-					<!-- wp:group {"style":{"spacing":{"blockGap":"1.5em"}},"layout":{"type":"constrained"}} -->
-					<div class="wp-block-group">
-						<!-- wp:separator {"backgroundColor":"cyan-bluish-gray"} -->
-						<hr class="wp-block-separator has-text-color has-cyan-bluish-gray-color has-alpha-channel-opacity has-cyan-bluish-gray-background-color has-background" />
-						<!-- /wp:separator -->
-						<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-						<div class="wp-block-group">
-							<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-							<div class="wp-block-group">
-								<!-- wp:paragraph -->
-								<p><strong>1.0.1</strong></p>
-								<!-- /wp:paragraph -->
-								<!-- wp:sortable/date {"textColor":"vivid-purple"} /-->
-							</div>
-							<!-- /wp:group -->
-							<!-- wp:list {"fontSize":"normal"} -->
-							<ul class="has-normal-font-size">
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> allow Group block in Sortable Entry;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> detect time format in Sortable Date block;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Update:</strong> improve Insert buttons in the editor;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Fix:</strong> style back-end issues;</li>
-								<!-- /wp:list-item -->
-								<!-- wp:list-item -->
-								<li><strong>Fix:</strong> wrong time output in Sortable Date block when viewed on front-end;</li>
-								<!-- /wp:list-item -->
-							</ul>
-							<!-- /wp:list -->
-						</div>
-						<!-- /wp:group -->
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:sortable/entry -->
-				<!-- wp:sortable/entry {"dateTime":"2022-05-05T10:21:00"} -->
-				<div class="wp-block-sortable-entry">
-					<!-- wp:group {"style":{"spacing":{"blockGap":"1.5em"}},"layout":{"type":"constrained"}} -->
-					<div class="wp-block-group">
-						<!-- wp:separator {"backgroundColor":"cyan-bluish-gray"} -->
-						<hr class="wp-block-separator has-text-color has-cyan-bluish-gray-color has-alpha-channel-opacity has-cyan-bluish-gray-background-color has-background" />
-						<!-- /wp:separator -->
-						<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-						<div class="wp-block-group">
-							<!-- wp:group {"style":{"spacing":{"blockGap":"1em"}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-							<div class="wp-block-group">
-								<!-- wp:paragraph -->
-								<p><strong>1.0.0</strong></p>
-								<!-- /wp:paragraph -->
-								<!-- wp:sortable/date {"textColor":"vivid-purple"} /-->
-							</div>
-							<!-- /wp:group -->
-							<!-- wp:list {"fontSize":"normal"} -->
-							<ul class="has-normal-font-size">
-								<!-- wp:list-item -->
-								<li>Initial release.</li>
-								<!-- /wp:list-item -->
-							</ul>
-							<!-- /wp:list -->
-						</div>
-						<!-- /wp:group -->
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:sortable/entry -->
-			</div>
-			<!-- /wp:sortable/container -->',
-		)
+	// Get availible patterns.
+	$pattern_namespaces = glob( dirname( __FILE__ ) . '/patterns/*' );
+
+	// Get patterns namespace only.
+	$pattern_namespaces = array_map(
+		function( $type_path ) {
+			return basename( $type_path, '.php' );
+		},
+		$pattern_namespaces
 	);
+
+	// Registers block patterns.
+	foreach ( $pattern_namespaces as $pattern_namespace ) {
+		$pattern_file = __DIR__ . '/patterns/' . $pattern_namespace . '.php';
+		register_block_pattern(
+			'sortable/' . $pattern_namespace,
+			require $pattern_file
+		);
+	}
 }
 add_action( 'init', 'sortable_plugin_init' );
 
