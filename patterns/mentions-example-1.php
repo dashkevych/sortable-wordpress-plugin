8fc632ee478273096aa7e6c456b06f2f20acbc14
Taras Dashkevych <trdashix@gmail.com>
2023-10-11 13:26:31 -0700

    * dev: update formatting;

--- patterns/mentions-example-1.php
+++ patterns/mentions-example-1.php
@@ -10,84 +10,87 @@ return array(
 	'description' => _x( 'Layout to create Media Mentions layout.', 'Block pattern description', 'sortable' ),
 	'blockTypes'  => array( 'sortable/container' ),
 	'content'     => '<!-- wp:sortable/container {"layout":"grid","columns":2,"separator":{"color":"#0000004f","style":"solid","width":"1px"}} -->
-	<div class="wp-block-sortable-container is-grid columns-2 has-separator" style="--wp--sortable-container--separator--width:1px;--wp--sortable-container--separator--style:solid;--wp--sortable-container--separator--color:#0000004f"><!-- wp:sortable/entry {"dateTime":"2023-04-13T18:56:00"} -->
-	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-	<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
-
-	<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
-	<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Japan Times</h2>
-	<!-- /wp:heading --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x960.webp" alt="" class="wp-image-121"/></figure>
-	<!-- /wp:image --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
-	<p class="has-link-color has-small-font-size"><a href="#">Read about publication</a></p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:sortable/entry -->
-
-	<!-- wp:sortable/entry {"dateTime":"2021-09-04T18:56:00"} -->
-	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-	<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
-
-	<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
-	<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Washington Post</h2>
-	<!-- /wp:heading --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x960.webp" alt="" class="wp-image-121"/></figure>
-	<!-- /wp:image --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
-	<p class="has-link-color has-small-font-size"><a href="#">Read about publication</a></p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:sortable/entry -->
-
-	<!-- wp:sortable/entry {"dateTime":"2021-01-24T18:56:00"} -->
-	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-	<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
-
-	<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
-	<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">The Times</h2>
-	<!-- /wp:heading --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x960.webp" alt="" class="wp-image-121"/></figure>
-	<!-- /wp:image --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
-	<p class="has-link-color has-small-font-size"><a href="#">Read about publication</a></p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:sortable/entry -->
-
-	<!-- wp:sortable/entry {"dateTime":"2014-02-22T18:56:00"} -->
-	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-	<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)"><!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
-
-	<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
-	<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Kyiv Post</h2>
-	<!-- /wp:heading --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
-	<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x960.webp" alt="" class="wp-image-121"/></figure>
-	<!-- /wp:image --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
-	<p class="has-link-color has-small-font-size"><a href="#">Read about publication</a></p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:sortable/entry --></div>
+	<div class="wp-block-sortable-container is-grid columns-2 has-separator" style="--wp--sortable-container--separator--width:1px;--wp--sortable-container--separator--style:solid;--wp--sortable-container--separator--color:#0000004f">
+		<!-- wp:sortable/entry {"dateTime":"2023-04-13T18:56:00"} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)">
+				<!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
+				<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
+				<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Japan Times</h2>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x880.webp" alt="" class="wp-image-121" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
+		<p class="has-link-color has-small-font-size"><a href="#">Read more</a></p>
+		<!-- /wp:paragraph -->
+		<!-- /wp:sortable/entry -->
+		<!-- wp:sortable/entry {"dateTime":"2021-09-04T18:56:00"} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)">
+				<!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
+				<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
+				<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Washington Post</h2>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x880.webp" alt="" class="wp-image-121" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
+		<p class="has-link-color has-small-font-size"><a href="#">Read more</a></p>
+		<!-- /wp:paragraph -->
+		<!-- /wp:sortable/entry -->
+		<!-- wp:sortable/entry {"dateTime":"2021-01-24T18:56:00"} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)">
+				<!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
+				<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
+				<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">The Times</h2>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x880.webp" alt="" class="wp-image-121" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
+		<p class="has-link-color has-small-font-size"><a href="#">Read more</a></p>
+		<!-- /wp:paragraph -->
+		<!-- /wp:sortable/entry -->
+		<!-- wp:sortable/entry {"dateTime":"2014-02-22T18:56:00"} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em","margin":{"bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group" style="margin-bottom:var(--wp--preset--spacing--40)">
+				<!-- wp:sortable/date {"style":{"color":{"text":"#000000b3"}},"fontSize":"small"} /-->
+				<!-- wp:heading {"placeholder":"Enter title...","textColor":"foreground","fontSize":"x-large"} -->
+				<h2 class="wp-block-heading has-foreground-color has-text-color has-x-large-font-size">Kyiv Post</h2>
+				<!-- /wp:heading -->
+			</div>
+			<!-- /wp:group -->
+			<!-- wp:image {"id":121,"sizeSlug":"large","linkDestination":"none"} -->
+			<figure class="wp-block-image size-large"><img src="' . SORTABLE_BLOCK_PLUGIN_DIR_URL . '/assets/images/640x880.webp" alt="" class="wp-image-121" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:group -->
+		<!-- wp:paragraph {"placeholder":"Enter description...","style":{"elements":{"link":{"color":{"text":"var:preset|color|vivid-red"},":hover":{"color":{"text":"var:preset|color|foreground"}}}}},"fontSize":"small"} -->
+		<p class="has-link-color has-small-font-size"><a href="#">Read more</a></p>
+		<!-- /wp:paragraph -->
+		<!-- /wp:sortable/entry -->
+	</div>
 	<!-- /wp:sortable/container -->',
 );
